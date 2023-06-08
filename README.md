7700717d2f747bf243a845734abdce88dbcc7cc4
sameeryalla <sameeryalla@gmail.com>
2023-06-08 23:01:09 +0530

    Raghu code taken as back up

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# tf-module-amazon
-tf-module-amazon
+# tf-module-amazon-mq
---
+++ userdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /opt
+git clone https://github.com/sameeryalla/roboshop-shell
+cd roboshop-shell
+bash rabbitmq.sh ${rabbitmq_appuser_password} &>>/opt/roboshop.log
+
